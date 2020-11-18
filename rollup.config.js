a3887174041a7570b2df85156d528869548f5789
Allison Fredrickson <gaballison@users.noreply.github.com>
2020-11-18 13:10:30 -0500

    Cleanup extraneous files

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-// rollup.config.js
-export default {
-    input: 'src/js/app.js',
-    output: {
-      file: 'js/bundle.js',
-      format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-	  sourcemap: true
-    }
-  };
